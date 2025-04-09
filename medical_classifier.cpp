ac5294590bdcb93d0700d23b7ead5140c5a8198f
Ishaan Datta <ishaandatta19@gmail.com>
2025-04-09 10:52:33 +0530

    Add: Added batch printing

--- medical_classifier.cpp
+++ medical_classifier.cpp
@@ -5,13 +5,68 @@
 #include "activations.hpp"
 #include "pooling.hpp"
 #include "losses.hpp"
+#include "dataloader.hpp"
 #include <iostream>
 #include <vector>
 #include <memory>
 #include <random>
 #include <algorithm>
-#include "dataloader.hpp"
 using namespace std;
+
+double eval(Network network, DataLoader val_loader, int num_batches = 5){
+	val_loader.reset();
+	val_loader.shuffle_data(); // Get different batches each time
+	double overall_loss = 0;
+	double loss;
+	for (int i = 0 ; i < num_batches; i++){
+		auto [val_x, val_label] = val_loader.get_next_batch();
+		for (int sample = 0; sample < val_x.size(); sample++){
+			auto pred = network.predict(val_x[sample]);
+			loss =  Loss::cross_entropy_loss(val_label[sample], pred);
+			overall_loss += loss;
+		}
+	}
+	overall_loss /= num_batches * val_loader.batch_size;
+	return overall_loss;
+}
+
+// Function to print batch information for debugging
+void print_batch_info(const std::vector<std::vector<Eigen::MatrixXd>>& batch_x, 
+                     const std::vector<std::vector<Eigen::MatrixXd>>& batch_y) {
+    // Print batch dimensions
+    cout << "Batch size: " << batch_x.size() << endl;
+    if (!batch_x.empty()) {
+        cout << "First sample channels: " << batch_x[0].size() << endl;
+        if (!batch_x[0].empty()) {
+            cout << "First channel dimensions: " << batch_x[0][0].rows() << "x" << batch_x[0][0].cols() << endl;
+            
+            // Print a small sample of the first channel
+            cout << "Sample of first channel (top-left 5x5):" << endl;
+            int rows = std::min(5, batch_x[0][0].rows());
+            int cols = std::min(5, batch_x[0][0].cols());
+            for (int i = 0; i < rows; i++) {
+                for (int j = 0; j < cols; j++) {
+                    cout << batch_x[0][0](i, j) << " ";
+                }
+                cout << endl;
+            }
+        }
+    }
+    
+    // Print label dimensions
+    cout << "Label batch size: " << batch_y.size() << endl;
+    if (!batch_y.empty()) {
+        cout << "First label channels: " << batch_y[0].size() << endl;
+        if (!batch_y[0].empty()) {
+            cout << "First label channel dimensions: " << batch_y[0][0].rows() << "x" << batch_y[0][0].cols() << endl;
+            cout << "First label values:" << endl;
+            for (int i = 0; i < batch_y[0][0].rows(); i++) {
+                cout << batch_y[0][0](i, 0) << " ";
+            }
+            cout << endl;
+        }
+    }
+}
 
 int main()
 {
@@ -23,7 +78,7 @@ int main()
 	auto test_batch = train_loader.get_next_batch();
 	train_loader.reset();
 	auto test = val_loader.get_next_batch();
-	cout << test.first << endl;
+	// cout << test.first << endl;
 	val_loader.reset();
 	cout << "Loaded successfully" << endl;
 
@@ -82,7 +137,7 @@ int main()
 
 	// Flatten the output for dense layers
 	// With 512 channels: 512 * 1 * 1 = 512
-	layers.push_back(std::make_shared<Reshape>(vector<int>{512, 1, 1}, vector<int>{512, 1, 1}));
+	layers.push_back(std::make_shared<Reshape>(vector<int>{512, 1, 1}, vector<int>{1, 512, 1}));
 
 	// Dense layers for classification
 	// Dense constructor takes input_size, output_size
@@ -99,81 +154,88 @@ int main()
 	Network network(layers);
 
 	// Set loss function
-	// network.set_loss<CrossEntropyLoss>();  // This line is causing the error
 	cout << "network init done successfully" << endl;
-
-	// Test the network layer by layer
-	cout << "\n=== Testing Network Layer by Layer ===\n";
-	
-	// Create a sample input (1 channel, 300x300)
-	std::vector<Eigen::MatrixXd> sample_input(1, Eigen::MatrixXd::Ones(300, 300));
-	cout << "Input shape: 1x" << sample_input[0].rows() << "x" << sample_input[0].cols() << endl;
-	
-	// Process through each layer and print dimensions
-	std::vector<Eigen::MatrixXd> current_output = sample_input;
-	
-	for (size_t i = 0; i < layers.size(); ++i) {
-		cout << "\nLayer " << i << ": ";
-		
-		// Get layer type
-		if (dynamic_cast<Convolutional*>(layers[i].get())) {
-			cout << "Convolutional";
-		} else if (dynamic_cast<ReLU*>(layers[i].get())) {
-			cout << "ReLU";
-		} else if (dynamic_cast<MaxPooling*>(layers[i].get())) {
-			cout << "MaxPooling";
-		} else if (dynamic_cast<GlobalAvgPooling*>(layers[i].get())) {
-			cout << "GlobalAvgPooling";
-		} else if (dynamic_cast<Reshape*>(layers[i].get())) {
-			cout << "Reshape";
-		} else if (dynamic_cast<Dense*>(layers[i].get())) {
-			cout << "Dense";
-		} else {
-			cout << "Unknown";
-		}
-		
-		// Process through this layer
-		current_output = layers[i]->forward(current_output);
-		
-		// Print output dimensions
-		cout << " -> Output shape: " << current_output.size() << " channels";
-		if (!current_output.empty()) {
-			cout << ", " << current_output[0].rows() << "x" << current_output[0].cols();
-		}
-		cout << endl;
-	}
-	
-	cout << "\nFinal output shape: " << current_output.size() << " channels";
-	if (!current_output.empty()) {
-		cout << ", " << current_output[0].rows() << "x" << current_output[0].cols();
-	}
-	cout << endl;
 
 	// Training code
 	cout << "\n=== Starting Training ===\n";
 	
 	// Training parameters
-	int epochs = 50;
+	int num_epochs = 100;
 	double learning_rate = 0.001;
 	bool verbose = true;
 	
-	// Prepare training data
-	std::vector<std::vector<Eigen::MatrixXd>> x_train;
-	std::vector<std::vector<Eigen::MatrixXd>> y_train;
-	
-	// Collect all training data
-	while (train_loader.has_next_batch()) {
-		auto [batch_inputs, batch_labels] = train_loader.get_next_batch();
-		x_train.insert(x_train.end(), batch_inputs.begin(), batch_inputs.end());
-		y_train.insert(y_train.end(), batch_labels.begin(), batch_labels.end());
+	double epoch_loss = 0;
+	for (int epoch = 0; epoch < num_epochs; epoch++){
+		cout << "Starting epoch: " << epoch << endl;
+		train_loader.reset();
+		epoch_loss = 0;
+		if (epoch < 10){
+			learning_rate = 0.005;
+		}
+		for (int batch_num = 0; batch_num < train_loader.get_num_batches(); batch_num++){
+			cout << "Batch num: " << batch_num << endl;
+			auto [batch_x, batch_y] = train_loader.get_next_batch();
+			
+			// Print batch information for debugging
+			print_batch_info(batch_x, batch_y);
+			
+			cout << "Starting training on batch" << endl;
+            network.train(batch_x, batch_y,
+                          Loss::cross_entropy_loss,
+                          Loss::cross_entropy_loss_prime,
+                          1, learning_rate, true);
+			cout << "Ending training on batch" << endl;
+		}
+		cout << "Getting val loss" << endl;
+		double val_loss = eval(network, val_loader, 8);
+		cout << "Epoch " << epoch << " / " << num_epochs << ": validation loss: " << val_loss << endl;
 	}
 	
-	// Train the network
-	network.train(x_train, y_train,
-		Loss::cross_entropy_loss, Loss::cross_entropy_loss_prime,
-		epochs, learning_rate, verbose);
+	double final_loss = eval(network, val_loader, val_loader.get_num_batches());
+	cout << "Final validation loss: " << final_loss << endl;
 	
 	cout << "Training completed successfully" << endl;
 	
 	return 0;
 }
+
+/* EARLIER CODE FOR TESTING*/
+	// // Process through each layer and print dimensions
+	// std::vector<Eigen::MatrixXd> current_output = sample_input;
+	
+	// for (size_t i = 0; i < layers.size(); ++i) {
+	// 	cout << "\nLayer " << i << ": ";
+		
+	// 	// Get layer type
+	// 	if (dynamic_cast<Convolutional*>(layers[i].get())) {
+	// 		cout << "Convolutional";
+	// 	} else if (dynamic_cast<ReLU*>(layers[i].get())) {
+	// 		cout << "ReLU";
+	// 	} else if (dynamic_cast<MaxPooling*>(layers[i].get())) {
+	// 		cout << "MaxPooling";
+	// 	} else if (dynamic_cast<GlobalAvgPooling*>(layers[i].get())) {
+	// 		cout << "GlobalAvgPooling";
+	// 	} else if (dynamic_cast<Reshape*>(layers[i].get())) {
+	// 		cout << "Reshape";
+	// 	} else if (dynamic_cast<Dense*>(layers[i].get())) {
+	// 		cout << "Dense";
+	// 	} else {
+	// 		cout << "Unknown";
+	// 	}
+		
+	// 	// Process through this layer
+	// 	current_output = layers[i]->forward(current_output);
+		
+	// 	// Print output dimensions
+	// 	cout << " -> Output shape: " << current_output.size() << " channels";
+	// 	if (!current_output.empty()) {
+	// 		cout << ", " << current_output[0].rows() << "x" << current_output[0].cols();
+	// 	}
+	// 	cout << endl;
+	// }
+	
+	// cout << "\nFinal output shape: " << current_output.size() << " channels";
+	// if (!current_output.empty()) {
+	// 	cout << ", " << current_output[0].rows() << "x" << current_output[0].cols();
+	// }
+	// cout << endl;
