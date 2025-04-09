1588e8315c1f63644ca3b9d9d909a56cac1aff79
Ishaan Datta <ishaandatta19@gmail.com>
2025-04-09 10:51:44 +0530

    Refactor: Included image folder defns. and functionality in dataloader itself, to avoid include errors

--- dataloader.hpp
+++ dataloader.hpp
@@ -1,10 +1,50 @@
 #pragma once
-
 #include <Eigen/Dense>
 #include <vector>
 #include <random>
 #include <algorithm>
-#include "image_loader.hpp"
+#include "stb_image/stb_image.h"
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string>
+#include <filesystem>
+using namespace std;
+
+typedef struct ImageStruct
+{
+	int channels;
+	int height;
+	int width;
+	string file_path;
+	std::shared_ptr<std::vector<Eigen::MatrixXd>> actual_image; 
+	string label;
+} ImageStruct;
+
+class ImageFolder
+{
+	public:
+		// Initialise image folder with all the images from the nested directory structure
+		// Only relevant method of class
+		ImageFolder(string folder_root);
+		unordered_map<string, int> get_label_counts();
+		std::vector<Eigen::MatrixXd> raw_img_to_matrix(unsigned char* img, int channels, int width, int height);
+		~ImageFolder(); // Destructor to avoid memory leaks
+		string root_folder_path;
+		vector<string> labels;
+		int num_classes;
+		
+		// How images are stored:
+		// 4 dimensions: 1 - which label, 2 - which image in label,
+		// 3(shared ptr) - which channel in image, 
+		// 4 - actual matrix containing pixel values for that image
+		vector<vector<std::shared_ptr<std::vector<Eigen::MatrixXd>>>> images; // shared ptr makes our life way easier
+		
+		// Store image metadata by image
+		// Not separated by channels obviously, so dimensions only are 1. Label, 2. Image
+		vector<vector<ImageStruct>> images_data;
+		unordered_map<string, int> label_counts;
+};
 
 class DataLoader {
 public:
@@ -37,6 +77,7 @@ public:
 	int  get_num_batches() const;
 	int batch_size;
 	int num_classes;
+	void shuffle_data();
 
 private:
 	// data[i].first  = vector<Eigen::MatrixXd>  → channels of image i
@@ -52,6 +93,5 @@ private:
 	int current_batch;
 	int num_batches;
 
-	void shuffle_data();
 	std::vector<Eigen::MatrixXd> one_hot_encode(int label);
 };
